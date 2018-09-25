b5e6353b3765551fb176a34fcf10f6d807e2f42c
kuriyantoadi <kuryantoadi8@gmail.com>
2018-09-25 10:02:55 +0700

    25 sep 2018, updatedari server

--- install-xampp/install-webserver.sh
+++ install-xampp/install-webserver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Program Menginstall xampp secara online"
-wget http://download1338.mediafire.com/kec7xxpx01mg/5v42mqskuop5z9l/xampp-linux-x64-5.6.30-0-installer.run
-sudo chmod +x xampp-linux-x64-5.6.30-0-installer.run
-sudo ./xampp-linux-x64-5.6.30-0-installer.run
+#wget http://download1338.mediafire.com/kec7xxpx01mg/5v42mqskuop5z9l/xampp-linux-x64-5.6.30-0-installer.run
+#sudo chmod +x xampp-linux-x64-5.6.30-0-installer.run
+#sudo ./xampp-linux-x64-5.6.30-0-installer.run
 
-sudo /opt/lampp/lampp start
+#sudo /opt/lampp/lampp start
 
 sudo rm -rf /opt/lampp/htdocs/*
 
