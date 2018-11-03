c9fc82e07146ee0a4cc1019f3bbd596045ce9ad0
kuriyantoadi <melatikuning212@gmail.com>
2018-11-03 22:17:01 +0700

    update 03 nov 2018, update27

--- beta/webserver/cek-webserver.sh
+++ beta/webserver/cek-webserver.sh
@@ -3,10 +3,10 @@ then
   #echo -e "Hapus file Index                                [\033[1;31m Gagal \033[0m    ]"
   echo -e "Tidak perlu melakukan clone                      [\033[1;32m Berhasil \033[0m ]"
 
-elif[ 2 = $webserver]
+elif [ 2 = $webserver]
   #Pengecekan Git
   if ! [ -x "$(command -v git)" ]; then
-    echo -e "Git sudah terinstall                            [\033[1;31m Gagal \033[0m ]" >&2
+    echo -e "Git sudah terinstall                           [\033[1;31m Gagal \033[0m ]" >&2
     exit 1
   else
     echo -e "Git sudah terinstall                           [\033[1;32m Berhasil \033[0m ]"
