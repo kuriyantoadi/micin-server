fb481a05ff9868a80390ba9a85732b3bc5ef5e82
kuriyantoadi <melatikuning212@gmail.com>
2018-11-03 22:24:13 +0700

    update 03 nov 2018, update29

--- beta/webserver/cek-webserver.sh
+++ beta/webserver/cek-webserver.sh
@@ -1,27 +1,26 @@
-if [ 1 = $webserver ]
-then
-  #echo -e "Hapus file Index                                [\033[1;31m Gagal \033[0m    ]"
-  echo -e "Tidak perlu melakukan clone                      [\033[1;32m Berhasil \033[0m ]"
+case $webserver in
+	1 )
+    echo -e "Tidak perlu melakukan clone                      [\033[1;32m Berhasil \033[0m ]"
+		;;
 
-elif [ 2 = $webserver]
-then
+  2 )
   #Pengecekan Git
-  if ! [ -x "$(command -v git)" ]; then
-    echo -e "Git sudah terinstall                           [\033[1;31m Gagal \033[0m ]" >&2
-    exit 1
-  else
-    echo -e "Git sudah terinstall                           [\033[1;32m Berhasil \033[0m ]"
-  fi
+    if ! [ -x "$(command -v git)" ]; then
+      echo -e "Git sudah terinstall                           [\033[1;31m Gagal \033[0m ]" >&2
+      exit 1
+    else
+      echo -e "Git sudah terinstall                           [\033[1;32m Berhasil \033[0m ]"
+    fi
 
-  #Pengecekan folder smkrg-cbt
-  if [ -e /opt/lampp/htdocs/ujian.php  ]
-  then
-    echo -e "Menyalin smkrg-cbt                             [\033[1;32m Berhasil \033[0m ]"
+    #Pengecekan folder smkrg-cbt
+    if [ -e /opt/lampp/htdocs/ujian.php  ]
+    then
+      echo -e "Menyalin smkrg-cbt                             [\033[1;32m Berhasil \033[0m ]"
 
-  else
-    echo -e "Menyalin smkrg-cbt                             [\033[1;31m Gagal \033[0m    ]"
-  fi
-
-else
-  echo ""
-fi
+    else
+      echo -e "Menyalin smkrg-cbt                             [\033[1;31m Gagal \033[0m    ]"
+    fi
+	*)
+		echo "tidak dikenali"
+		;;
+esac
